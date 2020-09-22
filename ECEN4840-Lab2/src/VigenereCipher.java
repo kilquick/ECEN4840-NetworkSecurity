5ec5160ced3b259c4271f2808ce1a15ddf0129d7
Tyler Zoucha <tylerzoucha@gmail.com>
2020-09-21 20:56:17 -0500

    Init git

---
+++ ECEN4840-Lab2/src/VigenereCipher.java
@@ -0,0 +1,39 @@
+public class VigenereCipher {
+    public static String encrypt(String text, final String key) {
+        StringBuilder res = new StringBuilder();
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z')
+                continue;
+            res.append((char) ((c + key.charAt(j) - 2 * 'A') % 26 + 'A'));
+            j = ++j % key.length();
+        }
+        return res.toString();
+    }
+
+    public static String decrypt(String text, final String key)
+    {
+        StringBuilder res = new StringBuilder();
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++)
+        {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z')
+                continue;
+            res.append((char) ((c - key.charAt(j) + 26) % 26 + 'A'));
+            j = ++j % key.length();
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args)
+    {
+        String key = "NETWORK";
+        String message = "ITISTIMETOTAKESECURITYSERIOUSLY ";
+        String encryptedMsg = encrypt(message, key);
+        System.out.println("String: " + message);
+        System.out.println("Encrypted message: " + encryptedMsg);
+        System.out.println("Decrypted message: " + decrypt(encryptedMsg, key));
+    }
+}
