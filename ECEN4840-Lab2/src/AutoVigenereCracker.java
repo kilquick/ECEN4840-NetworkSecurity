f266b6114c79ecd3f0c0b972b13a48bff5ff5be1
Tyler <tylerzoucha@gmail.com>
2020-09-24 02:06:33 -0500

    Git it, git

--- ECEN4840-Lab2/src/AutoVigenereCracker.java
+++ ECEN4840-Lab2/src/AutoVigenereCracker.java
@@ -1,7 +1,7 @@
 public class AutoVigenereCracker {
     public static void main(String[] args) {
         String keypool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-        String plain = "mr mustard with the candlestick in the hall";
+        String plain = "YOUR PACKAGE WILL BE READY BY FRIDAY";
         String cipher = "ANKYODKYUREPFJBYOJDSPLREYIUNOFDOIUERFPLUYTS";
 
         System.out.println(getDecryption(plain, keypool, cipher));
@@ -12,7 +12,7 @@ public class AutoVigenereCracker {
         plaintxt = plaintxt.toUpperCase();
         StringBuilder sb = new StringBuilder();
         for (int i = 0, j = 0; i < plaintxt.length() - 1; i++) {
-            int k = ((ciphertxt.charAt(i) - plaintxt.charAt(i) + 27));
+            int k = ((-plaintxt.charAt(i) + ciphertxt.charAt(i) % 27));
             while (k < 0) {
                 k += 27;
             }
@@ -25,10 +25,6 @@ public class AutoVigenereCracker {
             }
             j = ++j % keys.length();
         }
-     /*   for (int index = 0; index < sb.length(); index++)
-            if ( sb.charAt(index) == '[') {
-                sb.setCharAt(index, ' ');
-            }*/
         return sb.toString();
     }
 }
