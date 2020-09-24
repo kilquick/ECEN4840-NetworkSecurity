6bba40067e907f2b1a93b7a3b12c10008991f7da
Tyler <tylerzoucha@gmail.com>
2020-09-24 02:18:33 -0500

    Git it, git

--- ECEN4840-Lab2/src/AutoVigenereCracker.java
+++ ECEN4840-Lab2/src/AutoVigenereCracker.java
@@ -1,10 +1,16 @@
 public class AutoVigenereCracker {
     public static void main(String[] args) {
         String keypool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-        String plain = "YOUR PACKAGE WILL BE READY BY FRIDAY";
+        String plaintxt1 = "YOUR PACKAGE WILL BE READY BY FRIDAY";
+        String plaintxt2 = "LOST SCARLET WITH THE KNIFE IN THE LIBRARY";
+        String plaintxt3 = "MEET ME ON SUNDAY AT SIX AM";
+        String plaintxt4 = "MR MUSTARD WITH THE CANDLESTICK IN THE HALL";
         String cipher = "ANKYODKYUREPFJBYOJDSPLREYIUNOFDOIUERFPLUYTS";
 
-        System.out.println(getDecryption(plain, keypool, cipher));
+        System.out.println(getDecryption(plaintxt1, keypool, cipher));
+        System.out.println(getDecryption(plaintxt2, keypool, cipher));
+        System.out.println(getDecryption(plaintxt3, keypool, cipher));
+        System.out.println(getDecryption(plaintxt4, keypool, cipher));;
     }
 
     static String getDecryption(String plaintxt, String keys, String ciphertxt) {
@@ -12,7 +18,7 @@ public class AutoVigenereCracker {
         plaintxt = plaintxt.toUpperCase();
         StringBuilder sb = new StringBuilder();
         for (int i = 0, j = 0; i < plaintxt.length() - 1; i++) {
-            int k = ((-plaintxt.charAt(i) + ciphertxt.charAt(i) % 27));
+            int k = (((-plaintxt.charAt(i) + ciphertxt.charAt(i)) % 27));
             while (k < 0) {
                 k += 27;
             }
