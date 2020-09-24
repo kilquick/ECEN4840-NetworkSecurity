4715a9c01d2a57f22696e9ab21de8ccee16a8a7c
Tyler <tylerzoucha@gmail.com>
2020-09-24 01:42:58 -0500

    Working?

--- ECEN4840-Lab2/src/AutoVigenereCracker.java
+++ ECEN4840-Lab2/src/AutoVigenereCracker.java
@@ -1,7 +1,7 @@
 public class AutoVigenereCracker {
     public static void main(String[] args) {
         String keypool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-        String plain = "YOUR PACKAGE WILL BE READY BY FRIDAY";
+        String plain = "mr mustard with the candlestick in the hall";
         String cipher = "ANKYODKYUREPFJBYOJDSPLREYIUNOFDOIUERFPLUYTS";
 
         System.out.println(getDecryption(plain, keypool, cipher));
@@ -12,22 +12,23 @@ public class AutoVigenereCracker {
         plaintxt = plaintxt.toUpperCase();
         StringBuilder sb = new StringBuilder();
         for (int i = 0, j = 0; i < plaintxt.length() - 1; i++) {
-            int k = ciphertxt.charAt(i) - plaintxt.charAt(i);
-            if (k < 0) {
+            int k = ((ciphertxt.charAt(i) - plaintxt.charAt(i) + 27));
+            while (k < 0) {
                 k += 27;
             }
-            while (k < 0) ;
-            if (k > 26) {
+            while (k > 26) {
                 k -= 27;
             }
-            while (k > 26) ;
-            sb.append((char) ((keys.charAt(k)) % 27 + 'A'));
+            sb.append(keys.charAt(k));
+            if (sb.charAt(i) == '[') {
+                sb.setCharAt(i, ' ');
+            }
             j = ++j % keys.length();
         }
-        for (int index = 0; index < sb.length(); index++)
+     /*   for (int index = 0; index < sb.length(); index++)
             if ( sb.charAt(index) == '[') {
                 sb.setCharAt(index, ' ');
-            }
+            }*/
         return sb.toString();
     }
 }
